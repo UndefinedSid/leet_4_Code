d0a68761ef675d62015da89f4451a6b5e0e47cfc
Siddharth kumar <180748912+UndefinedSid@users.noreply.github.com>
2026-01-03 23:06:19 +0530

    Add solution for painting n x 3 grid problem
    Implement a solution to count ways to paint a n x 3 grid with constraints on adjacent colors using recursion and memoization.

---
+++ POTD/1411.cpp
@@ -0,0 +1,70 @@
+/*
+Problem -->> 1411 (HARD) -->> NO. OF WAYS TO PAINT N * 3 GRID
+Topics -->> RECURSION + MEMOIZATION i.e., DP
+
+You have a grid of size n x 3 and you want to paint each cell of the grid with exactly one of the three colors:
+Red, Yellow, or Green while making sure that no two adjacent cells have the same color 
+(i.e., no two cells that share vertical or horizontal sides have the same color).
+
+Given n the number of rows of the grid, 
+return the number of ways you can paint this grid. 
+As the answer may grow large, the answer must be computed modulo 109 + 7.
+
+n == grid.length
+1 <= n <= 5000
+*/
+
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+
+    vector<string> states = {
+        "RYG", "RGY", "RYR", "RGR",
+        "YRG", "YGR", "YGY", "YRY",
+        "GRY", "GYR", "GRG", "GYG"
+    };
+
+    vector<vector<int>> dp;
+
+    int check(int n, int prev) {
+        if (n == 0) return 1;
+
+        if (dp[n][prev] != -1)
+            return dp[n][prev];
+
+        int ans = 0;
+        string last = states[prev];
+
+        for (int curr = 0; curr < 12; curr++) {
+            if (curr == prev) continue;
+
+            string cur = states[curr];
+            bool ok = true;
+
+            for (int col = 0; col < 3; col++) {
+                if (cur[col] == last[col]) {
+                    ok = false;
+                    break;
+                }
+            }
+            if (ok) {
+                ans = (ans + check(n - 1, curr)) % MOD;
+            }
+        }
+
+        return dp[n][prev] = ans;
+    }
+
+    int numOfWays(int n) {
+        dp.assign(n, vector<int>(12, -1));
+
+        int ans = 0;
+        for (int i = 0; i < 12; i++) {
+            ans = (ans + check(n - 1, i)) % MOD;
+        }
+
+        return ans;
+    }
+};
+
+// Time Complexity -->> O(N)  
