e1f575975aceaf78c4532dbdf891b68e8326b2b4
Siddharth kumar <180748912+UndefinedSid@users.noreply.github.com>
2026-01-22 23:35:48 +0530

    Create 287.cpp

---
+++ POTD/287.cpp
@@ -0,0 +1,62 @@
+/*   Scroll down to see JAVA code also      */
+/*
+      PROBLEM --> 287 (MEDIUM) --> FIND THE DUPLICATE NUMBER
+      TOPICS --> ARRAY, TWO POINTER, BINARY SEARCH, BIT MANIPULATION
+      // Company Tags  : AMAZON
+      Leetcode Link : https://leetcode.com/problems/find-the-duplicate-number/
+*/
+
+//NOTE : You can solve this using O(n) space, or in O(n^2) Time Complexity using double for loop to find the duplicate number
+//Follow Up : How can we prove that at least one duplicate number must exist in nums? - See My Video
+//Follow Up : Can you solve the problem in linear runtime complexity? - Below is the code for that
+
+/**************************************** C++ ****************************************/
+//T.C : O(n) Using "Hare & tortoise technique"
+//S.C : O(1)
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        
+        slow = nums[0];
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return slow;
+    }
+};
+
+
+/**************************************** JAVA ****************************************/
+//T.C : O(n) Using "Hare & tortoise technique"
+//S.C : O(1)
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        fast = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+}
