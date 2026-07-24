2bee8b90cf2195d8cfae002be25eafaff7b59b69
Siddharth kumar <iamsid0654@gmail.com>
2026-07-24 12:21:57 +0530

    Array 3514-number-of-unique-xor-triplets-ii 07-24-2026 2152 ms (26.4%) Medium

---
+++ C++/Medium/3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n = nums.size();
+        unordered_set<int> st;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                st.insert(nums[i] ^ nums[j]);
+            }
+        }
+        unordered_set<int> ans;
+        for(auto& val : st){
+            for(int i=0;i<n;i++){
+                ans.insert(nums[i] ^ val);
+            }
+        }
+        return ans.size();
+    }
+};
