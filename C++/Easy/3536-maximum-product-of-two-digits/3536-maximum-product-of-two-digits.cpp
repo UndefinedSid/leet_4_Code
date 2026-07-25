3534c70b47dabb23c12f7114e3ddb46d3d316039
Siddharth kumar <iamsid0654@gmail.com>
2026-07-25 15:12:41 +0530

    Math 3536-maximum-product-of-two-digits 07-25-2026 0 ms (100%) Easy

--- C++/Easy/3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ C++/Easy/3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int maxProduct(int n) {
+        /*  // brute force approach -> O(d^2)
+
         string str=to_string(n);
         int maxi=INT_MIN;
 
@@ -13,5 +15,36 @@ public:
             }
         }
         return (maxi==INT_MIN) ? 0 : maxi;
+
+        */
+
+        /* 
+        // less code approach->O(d log d)
+
+        string str=to_string(n);
+
+        sort(str.rbegin(),str.rend());
+
+        int ans=(str[0]-'0') * (str[1]-'0');
+        return ans;
+
+        */
+
+        // optimal approach -> O(d)
+
+        int maxi=0;
+        int maxi2=0;
+
+        while(n > 0){
+            int dgt=n % 10;
+            if(dgt > maxi){
+                maxi2=maxi;
+                maxi=dgt;
+            }else if(dgt > maxi2)
+                maxi2=dgt;
+
+            n /= 10;
+        }
+        return maxi * maxi2;
     }
 };
