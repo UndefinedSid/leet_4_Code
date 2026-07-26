4f82ce6ac56b01690c02220ccf7b789dc76f1c3a
Siddharth kumar <iamsid0654@gmail.com>
2026-07-26 16:22:43 +0530

    Array 0628-maximum-product-of-three-numbers 07-26-2026 24 ms (6.63%) Easy

---
+++ C++/Easy/0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.rbegin(),nums.rend());
+        int ans=1;
+        if(nums[n-1] >= 0 || n==3 || nums[0] < 0){
+            ans = (nums[0] * nums[1] * nums[2]);   
+        }
+        // else if(nums[0] < 0){
+        //     ans *= (nums[n-1] * nums[n-2] * nums[n-3]);
+        // }
+        else{
+            int l=1,r=n-1,neg=0;
+            ans=nums[0];
+            bool useRight=(nums[r] * nums[r-1] > nums[l] * nums[l+1]);
+            while(neg < 2){
+                if(useRight)
+                    ans *= nums[r--];
+                else
+                    ans *= nums[l++];
+
+                neg++;
+            }
+        }
+        return ans;
+    }
+};
