e8b776d615e09544d9fe1af30850ee4e800e871e
Siddharth kumar <iamsid0654@gmail.com>
2026-07-31 19:02:01 +0530

    Two Pointers 0680-valid-palindrome-ii 07-31-2026 3 ms (67.45%) Easy

---
+++ C++/Easy/0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int n;
+    bool finder(string &s,int i,int j,bool flag){
+        while(i < j){
+            if(s[i]!= s[j]){
+                if(flag)
+                    return false;
+                    
+               return finder(s,i+1,j,true) || finder(s,i,j-1,true);
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        n=s.size();
+        return finder(s,0,n-1,false);
+    }
+};
