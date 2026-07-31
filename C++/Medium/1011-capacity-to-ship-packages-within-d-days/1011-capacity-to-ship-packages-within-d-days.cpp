978f8fbb6c32f77df040ad803e4abd7262c7c170
Siddharth kumar <iamsid0654@gmail.com>
2026-08-01 00:51:12 +0530

    Array 1011-capacity-to-ship-packages-within-d-days 08-01-2026 11 ms (64.79%) Medium

---
+++ C++/Medium/1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int n;
+    bool finder(vector<int>& weights, int mid, int days) {
+        int currWt = 0;
+        for (int wt : weights) {
+            if (wt > mid)
+                return false;
+
+            if (currWt + wt > mid) {
+                days--;
+                currWt = wt;
+            } else
+                currWt += wt;
+
+        }
+        return days > 0;
+    }
+
+    int shipWithinDays(vector<int>& weights, int days) {
+
+        n = weights.size();
+
+        int st = 0, end = 0;
+        for(int w : weights){
+            st =max(st,w);
+            end += w;
+        }
+        while (st < end) {
+            int mid = st + (end - st) / 2;
+            if (finder(weights, mid, days)) {
+                end=mid;
+            } else
+                st = mid +1;
+        }
+        return st;
+    }
+};
