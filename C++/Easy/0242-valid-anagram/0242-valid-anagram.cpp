f0aad477548525fbf8ffcfb525da4e5174100bd5
Siddharth kumar <iamsid0654@gmail.com>
2026-08-02 11:53:54 +0530

    Hash Table 0242-valid-anagram 08-02-2026 0 ms (100%) Easy

--- C++/Easy/0242-valid-anagram/0242-valid-anagram.cpp
+++ C++/Easy/0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        /*
+        // for normal english letters
         vector<int> freq(26, 0);
         if(s.size() != t.size())
             return false;
@@ -14,6 +16,25 @@ public:
             if (cnt > 0)
                 return false;
 
+        return true;
+
+        */
+        // when we have unicode characters i.e (alphabets,symbols,emoji) also then use just hash map instead of fixed size array
+
+        unordered_map<char,int> freq;
+
+        if(s.size() != t.size())
+            return false;
+
+        for(int i=0;i<s.size();i++){
+            freq[s[i]]++;
+            freq[t[i]]--;
+        }
+
+        for(auto& p : freq){
+            if(p.second > 0)
+                return false;
+        }
         return true;
     }
 };
