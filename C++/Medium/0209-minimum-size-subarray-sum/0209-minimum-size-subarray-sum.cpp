3698166f78c5fa54ac0245b73d9ab59c895d546c
Siddharth kumar <iamsid0654@gmail.com>
2026-08-15 23:22:16 +0530

    Array 0209-minimum-size-subarray-sum 08-15-2026 0 ms (100%) Medium

---
+++ C++/Medium/0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        int ans = INT_MAX;
+        int i = 0, j = 0;
+        int sum = 0;
+
+        while (j < n) {
+            sum += nums[j];
+            while (sum >= target) {
+                ans = min(ans, j - i + 1);
+                sum -= nums[i];
+                i++;
+            }
+            j++;
+        }
+        return (ans==INT_MAX) ? 0 : ans;
+    }
+};
