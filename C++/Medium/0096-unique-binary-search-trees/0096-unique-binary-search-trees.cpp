cf199ab1d66ddb47ed9e61d71b22c7748ea2acb2
Siddharth kumar <iamsid0654@gmail.com>
2026-08-22 16:16:44 +0530

    Math 0096-unique-binary-search-trees 08-22-2026 0 ms (100%) Medium

--- C++/Medium/0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ C++/Medium/0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,6 +1,41 @@
 class Solution {
 public:
+
+    // Top Down (Rec + Memo) Approach
+    // Time comp-> O(N^2) & space-> O(N) 
+
+    int dp[20];
+
+    int finder(int n){
+        if(n <= 1)
+            return 1;
+
+        if(dp[n] != -1)
+            return dp[n];
+
+        int ans=0;
+
+        for(int i=1;i<=n;i++){
+            int leftBst=finder(i-1);
+            int rightBst=finder(n-i);
+
+           ans += leftBst * rightBst;
+        }
+        return dp[n] = ans;
+    }
+
     int numTrees(int n) {
+        memset(dp,-1,sizeof(dp));
+
+        return finder(n);
+    }
+};
+
+    // --------------------------------------------------
+    /* Bottom Up Approach 
+        // T.C -> O(N^2) and Space comp-> O(N)
+
+    int numTrees(int n){
         vector<int> dp(n+1,0);
         dp[0]=1;
         dp[1]=1;
@@ -11,5 +46,9 @@ public:
             }
         }
         return dp[n];
+       
     }
+     
 };
+
+    */
