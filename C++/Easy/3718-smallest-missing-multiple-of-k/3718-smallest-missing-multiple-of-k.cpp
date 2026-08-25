6c7eb1e9ff0fd5dfe9674a6bdf7f82270471e5f6
Siddharth kumar <iamsid0654@gmail.com>
2026-08-25 12:29:57 +0530

    Array 3718-smallest-missing-multiple-of-k 08-25-2026 3 ms (38.06%) Easy

---
+++ C++/Easy/3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int maxi=*max_element(nums.begin(),nums.end());
+        int mini=*min_element(nums.begin(),nums.end());
+
+        unordered_set<int> st(nums.begin(),nums.end());
+
+        int mul=k;
+        while(st.count(mul)){
+            mul += k;
+        }
+        return mul;
+
+    }
+};
