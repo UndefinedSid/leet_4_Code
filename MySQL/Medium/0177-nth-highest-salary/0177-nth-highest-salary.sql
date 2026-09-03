0245d88c37fa53f09cb7e29f93c725411a05abef
Siddharth kumar <iamsid0654@gmail.com>
2026-09-03 16:36:19 +0530

    Database 0177-nth-highest-salary 09-03-2026 507 ms (43.16%) Medium

--- MySQL/Medium/0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ MySQL/Medium/0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,11 +1,11 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-    declare M INT;
-    set M=N-1;
+declare m int;
+set m=N-1;
   RETURN (
       # Write your MySQL query statement below.
         select distinct salary from Employee
-        order by salary desc
-        limit M,1
+        order by salary desc 
+        limit 1 offset m
   );
 END
