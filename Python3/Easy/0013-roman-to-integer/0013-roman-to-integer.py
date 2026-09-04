275392e247fae6d2d9553d26993e9feb63f041fa
Siddharth kumar <iamsid0654@gmail.com>
2026-09-04 14:33:57 +0530

    Hash Table 0013-roman-to-integer 09-04-2026 6 ms (46.28%) Easy

---
+++ Python3/Easy/0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,21 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        mp={
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
+        }
+        ans=0
+        n=len(s)
+        for i in range(n):
+            if i< n-1 and mp[s[i]] < mp[s[i+1]]:
+                ans -= mp[s[i]]
+            else:
+                ans += mp[s[i]]
+                
+        return ans
+
