1e3e43e9f1ebaad99f263fe5117b688b3476614c
Siddharth kumar <iamsid0654@gmail.com>
2026-09-05 12:44:44 +0530

    Array 3904-smallest-stable-index-ii 09-05-2026 1 ms (96.86%) Medium

---
+++ C++/Medium/3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> suffMin(n);
+        suffMin[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            suffMin[i]=min(suffMin[i+1],nums[i]);
+        }
+
+        int preMax=nums[0];
+        for(int i=0;i<n;i++){
+            preMax=max(preMax,nums[i]);
+
+            if(preMax - suffMin[i] <= k)
+                return i;
+        }
+        return -1;
+
+    }
+};
