faeefe3a798944667a86e02d0e43f5431a0ad852
Siddharth kumar <iamsid0654@gmail.com>
2026-09-08 01:29:02 +0530

    Database 0262-trips-and-users 09-08-2026 817 ms (11.17%) Hard

---
+++ MySQL/Hard/0262-trips-and-users/0262-trips-and-users.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+select request_at as Day,
+    round(
+        sum(case when status != 'completed' then 1 else 0 end) / count(*), 2 
+    ) as "Cancellation Rate"
+from trips
+    where client_id not in (select users_id from users where banned ='yes')
+        and driver_id not in (select users_id from users where banned= 'yes')
+        and request_at between '2013-10-01' and '2013-10-03'
+    group by request_at;
