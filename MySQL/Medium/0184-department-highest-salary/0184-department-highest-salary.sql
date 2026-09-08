7093fa2b656222a2135d01c96661f61006f655f0
Siddharth kumar <iamsid0654@gmail.com>
2026-09-08 16:23:01 +0530

    Database 0184-department-highest-salary 09-08-2026 634 ms (93.19%) Medium

--- MySQL/Medium/0184-department-highest-salary/0184-department-highest-salary.sql
+++ MySQL/Medium/0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,4 +1,8 @@
 # Write your MySQL query statement below
+
+/* 
+## normal approach
+
 SELECT d.name AS Department,e.name AS Employee,e.salary AS Salary
 FROM Employee e
 JOIN Department d ON e.departmentId=d.id
@@ -6,3 +10,18 @@ WHERE (e.departmentId,e.salary) IN (
     SELECT departmentId,MAX(salary) FROM Employee
     GROUP BY departmentId
 );
+
+ */
+
+ ## CTE approach
+
+ with cteResult as(
+    select d.name as department,
+            e.name as employee,
+            e.salary as salary,
+        dense_rank() over (partition by e.departmentId 
+                    order by e.salary desc) as rnk
+        from employee e
+        join department d on e.departmentId=d.id
+ )
+ select department,employee,salary from cteResult where rnk=1;
