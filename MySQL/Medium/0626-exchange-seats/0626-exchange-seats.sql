81c550dd5dbcd9439bdb20d200a3eb9d6550e3cb
Siddharth kumar <iamsid0654@gmail.com>
2026-09-10 14:32:24 +0530

    Database 0626-exchange-seats 09-10-2026 361 ms (72.84%) Medium

--- MySQL/Medium/0626-exchange-seats/0626-exchange-seats.sql
+++ MySQL/Medium/0626-exchange-seats/0626-exchange-seats.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
+/*
+
 select 
 case
     when id % 2 = 0 then id - 1
@@ -7,3 +9,15 @@ case
 end as id, student
 from seat
 order by id asc;
+
+*/
+
+# window function and coalesce approach
+## COALESCE is an SQL function that returns the first non-null value in a list of arguments. If the first argument is NULL, it evaluates the second; if that is also NULL, it continues down the list until it finds a value that is not NULL.
+
+select id,
+    case 
+    when id % 2 = 0 then (lag(student) over (order by id))
+    else coalesce(lead(student) over (order by id), student)
+    end as student
+from seat;
