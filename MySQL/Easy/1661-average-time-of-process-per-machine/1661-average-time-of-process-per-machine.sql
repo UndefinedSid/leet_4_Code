282ba50452e8ee27592d2b3aefb1223391df16ea
Siddharth kumar <iamsid0654@gmail.com>
2026-09-16 00:30:00 +0530

    Database 1661-average-time-of-process-per-machine 09-16-2026 275 ms (53.27%) Easy

---
+++ MySQL/Easy/1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -0,0 +1,23 @@
+# Write your MySQL query statement below
+
+/*
+## self join approach
+
+select a1.machine_id,
+    round(avg(a2.timestamp - a1.timestamp),3) as processing_time
+from activity a1
+    join activity a2
+    on a1.machine_id=a2.machine_id
+    and a1.process_id=a2.process_id
+where a1.activity_type='start' and a2.activity_type='end'
+group by a1.machine_id;
+
+*/
+
+## conditional Aggregation Approach
+
+select machine_id,round(sum(case when activity_type='end' then timestamp 
+        else - timestamp end) / count(distinct process_id), 3) 
+        as processing_time
+    from activity
+group by machine_id;
