8a821fb7fd82c8f9b445245f896ce054f9be84fd
Siddharth kumar <iamsid0654@gmail.com>
2026-09-24 11:21:17 +0530

    Database 1211-queries-quality-and-percentage 09-24-2026 390 ms (63.62%) Easy

--- MySQL/Easy/1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ MySQL/Easy/1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,13 +1,26 @@
 # Write your MySQL query statement below
-
+/*  CTE Approach
 with qualityCte as (
     select query_name,
     rating * 1.0 /position as ratio,
     case when rating < 3 then 1 else 0 end as is_poor
     from queries
-    -- where query_name is not null
+    where query_name is not null
 )
 select query_name, round(avg(ratio),2) as quality,
     round(avg(is_poor) * 100 ,2) as poor_query_percentage
  from qualityCte
  group by query_name;
+
+
+*/
+ ## without CTE
+
+select query_name,
+    round(avg(rating / position),2) as quality,
+    round(avg(
+        case when rating < 3 then 1 else 0 end 
+    ) * 100 ,2) as poor_query_percentage
+from queries 
+where query_name is not null
+group by query_name;
