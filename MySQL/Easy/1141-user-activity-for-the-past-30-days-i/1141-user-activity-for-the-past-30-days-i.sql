75908959d36b46c00645ec1bff7bb1d511cd3644
Siddharth kumar <iamsid0654@gmail.com>
2026-09-26 11:39:26 +0530

    Database 1141-user-activity-for-the-past-30-days-i 09-26-2026 490 ms (88.15%) Easy

--- MySQL/Easy/1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ MySQL/Easy/1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -1,4 +1,22 @@
 # Write your MySQL query statement below
-Select activity_date AS day,COUNT(DISTINCT user_id) AS active_users FROM Activity
-    WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
-    GROUP BY activity_date;
+/*. using DATEDIFF Approach
+
+select activity_date as day,
+    count(distinct user_id ) as active_users from activity
+    where DATEDIFF('2019-07-27',activity_date) >=0
+        and DATEDIFF('2019-07-27', activity_date) < 30
+group by activity_date;
+
+*/
+
+## using CTE approach
+
+with activityCTE as(
+    select distinct user_id,activity_date
+    from activity
+    where activity_date between '2019-06-28' and '2019-07-27'
+)
+select activity_date as day,
+    count(distinct user_id ) as active_users
+from activityCTE
+group by activity_date;
